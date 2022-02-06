9e9d174252f4a414325ab618a199235f136ccf05
Peter <zkpanhao@gmail.com>
2022-02-06 21:34:29 +0800

    finish

--- MergeBooks.vb
+++ MergeBooks.vb
@@ -4,7 +4,7 @@ Sub MergeBooks()
     'Dir的使用
 
     Dim PathStr As String, FileStr As String, File(), n As Integer
-    Dim ActiveB As Workbook, Headline As Byte, Namess As String, Cell As Range
+    Dim ActiveB As Workbook, Headline As Byte, Namess As String, Cell As Range,Tephead as String
     Dim vrtSelectedItem As Variant
 '第一种写法
 '    With Application.FileDialog(msoFileDialogFolderPicker)          '使用Application的Filedialog方法调出文件浏览对话框
@@ -36,24 +36,25 @@ Sub MergeBooks()
 '另一种写法结束
     Set ActiveB = ActiveWorkbook                               '把当前空表指定为ActiveB
     On Error Resume Next
-    Headline = Application.InputBox("输入待合并的表的标题行数:", "输入框", 1, , , , , 1)    '获取用户输入的标题行数
-    If Headline < 0 Then
+    Tephead = InputBox("输入待合并的表的标题行数:")    '获取用户输入的标题行数
+    If cbyte(Tephead) < 0 Then
         Exit Sub                                               '小于0退出程序
-    ElseIf Headline = False Then                               '选择取消退出程序
+    ElseIf Tephead = "" Then                               '选择取消退出程序
         Exit Sub
     End If
+    Headline=cbyte(Tephead)
     On Error GoTo 0
-    Range("A" & Headline & ":B" & Headline) = Array("工作簿", "工作表")    '留两列放来源表名
+    Range("A1:B1") = Array("工作簿", "工作表")    '留两列放来源表名
     Application.ScreenUpdating = False
     Application.Calculation = xlCalculationManual
     For k = 1 To n                                             '第一层循环，遍历选中的所有文件
         Namess = Dir(File(k))                                  '获取文件名
         Workbooks.Open (File(k))                               '打开文件
         ActiveB.Activate
-'       If Headline = 0 Then GoTo Line0                        '标题行为0时跳过粘贴标题行的步骤
+        If Headline = 0 Then GoTo Line0                        '标题行为0时跳过粘贴标题行的步骤
         If k = 1 Then Intersect(Workbooks(Namess).Worksheets(1).UsedRange, _
                 Workbooks(Namess).Worksheets(1).Rows("1:" & Headline)).Copy Cells(1, 3)    '使用intersect的原因是为了copy的目标可以定位到单元格
-'Line0:
+Line0:
         For i = 1 To Workbooks(Namess).Worksheets.Count        '第二层循环，遍历每个工作簿里的工作表
             With Workbooks(Namess).Worksheets(i).UsedRange
                 If .Rows.Count <= Headline Then GoTo Lines
