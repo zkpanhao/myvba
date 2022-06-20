123a86c32584a27668375e9b03cd82e554f5e153
Peter <72325266+zkpanhao@users.noreply.github.com>
2022-06-20 17:54:27 +0800

    Update 工作表筛选拆分.vb

--- 合并与拆分/工作表筛选拆分.vb
+++ 合并与拆分/工作表筛选拆分.vb
@@ -38,8 +38,8 @@ Sub 工作表拆分()
         With Worksheets(CStr(Keyword(n))).Rows(Headline + 1)
             Rng.Copy .Cells(1)
             .Cells(1) = Rng.Value
-            .Columns("A:P").AutoFit
         End With
+    Worksheets(CStr(Keyword(n))).Columns("A:P").AutoFit
     Next n
     Cells.AutoFilter
     Application.ScreenUpdating = True
